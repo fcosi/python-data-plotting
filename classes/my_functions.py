b04a7d8e66867e8474d42abfcf4d9699d4401ea2
Filippo G. Cosi <filippo.cosi@ds.mpg.de>
2020-07-03 14:36:14 +0200

    final changes

--- classes/my_functions.py
+++ classes/my_functions.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
 
 class Analysis():
     """
@@ -19,3 +22,88 @@ class Analysis():
         if title:
             ax.set_title(title)
         fig.show()
+        
+    def GaussianFit(self, data, bin_nr=False, printinfo=False, showmu=False, showsigma=False, savefig=False):
+        """
+        Fct to create one Gaussian fit
+        Args:
+            data: numpy 1darray or list
+        """
+        from scipy.optimize import curve_fit
+        # functions
+        def gaussian(x, mu, sigma):
+            """
+            Gaussian distribution
+            Args:
+                - x values
+                - mu
+                - sigma
+            Returns:
+                gaussian curve
+            """
+            return (1/(sigma*np.sqrt(2*np.pi))*np.exp(- ((x - mu)**2)/(2*sigma**2)))
+        
+        if not bin_nr:
+            binnr = int(np.sqrt(data.size))
+        elif type(bin_nr)!=int:
+            print("Attention: binnumber is not an int, using 30 as default value!")
+            binnr = 30
+        else:
+            binnr = bin_nr
+        
+        vals, binedge = np.histogram(data, bins=binnr, density=True)
+        
+        # to overcome wrong positioning of the bins, compute the width of one bin and subtract half of it it to the edges
+        bin_width = binedge[1] - binedge[0]
+        bincenter = binedge[1:]- bin_width/2
+        
+        # compute the fit and little information
+        opt_vals, cov_coeff = curve_fit(gaussian, bincenter, vals)
+        
+        if printinfo:
+            print("optimal expected value mu: {}\noptimal standar deviation sigma: {}".format(*opt_vals))
+        
+        # =====================================================
+        
+        # plot everything
+        mu, sigma = opt_vals[0], opt_vals[1]
+        gauss_fit = gaussian(bincenter, *opt_vals)
+        max_gauss = max(gauss_fit)
+        
+        # define the plot
+        fig, ax = plt.subplots(1,1, figsize=(9,6), tight_layout=True)
+        
+        ax.hist(data, bins=binnr, density=True, label="distribution")
+        ax.plot(bincenter, vals, "ro")
+        
+        label_text="$\mu$={:.3}, $\sigma^2$={:.3}".format(*opt_vals)
+        ax.plot(bincenter, gauss_fit, 'r-', label=label_text)
+        
+        ax.set_xlabel("data")
+        ax.legend()
+        
+        # find value for half max gauss
+        half_max_idx = np.abs(gauss_fit - max_gauss/2).argmin()
+
+        starting_bin = bincenter[half_max_idx]
+        if starting_bin < mu:
+            xmin, xmax = starting_bin, starting_bin + 2*sigma
+        else:
+            xmax, xmin = starting_bin, starting_bin - 2*sigma
+        
+        # # plot mu and sigma
+        if showmu:
+            ax.vlines(x=mu, color="grey", linestyle="--", ymin=0, ymax=max_gauss)
+        if showsigma:
+            ax.hlines(y=max_gauss/2, xmin=xmin, xmax=xmax, color="grey", linestyle="--")
+            
+        if savefig:
+            fig.savefig("gaussian_fit_{}.pdf".format(savefig))
+        else:
+            fig.show()
+            
+    def ExponentianFit(self, data, args):
+        """
+        Fct for plotting an exponential fit to data
+        """
+        
--- multiple-gauss-fits.py
+++ multiple-gauss-fits.py
@@ -9,28 +9,20 @@ script to apply gaussian fit to all fire data
 import numpy as np
 import os
 
-from importlib import reload
-
 # import CLASS
-# from classes import my_functions
+from classes import my_functions
 
-# load data into dictionary
 forest_dict = {}
-for name in os.listdir("data/"):
-    if name.endswith(".npy"):
-        forest_dict[name.split(".")[0]] = np.array(np.load("data/{}".format(name), allow_pickle=True))
+for filename in os.listdir("data/"):
+    if filename.endswith(".npy"):
+        forest_dict[filename.split(".")[0]] = np.array(np.load("data/{}".format(filename), allow_pickle=True))
     else:
         continue
-    
-# apply the gauss fit
 
+cl = my_functions.Analysis()
 
-reload(my_functions)
+# testing the GaussFit()
+# cl.GaussianFit(forest_dict["forest_temp"], printinfo=True, bin_nr=30, showmu=True, savefig="dummy_save_name")
 
-cl = testing_class.Testing() # set here proper name
-cl.GaussianFit
-
-#
-#for name in forest_dict.keys():
-#    print(name)
-#    cl.GaussianFit(forest_dict[name])
+for name in forest_dict.keys():
+    cl.GaussianFit(forest_dict[name], showmu=True, savefig=name)
